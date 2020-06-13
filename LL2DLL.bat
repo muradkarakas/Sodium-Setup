cff5de5000105024d05e2b4fb0a55c22e824d5c9
Murad Karakaş <muradkarakas@gmail.com>
2020-06-13 14:53:22 +0300

    .

--- LL2DLL.bat
+++ LL2DLL.bat
@@ -2,5 +2,14 @@
 rem Example Usage: ll2dll C:\Sodium\Sodium-Setup\Sodium-Site\apps core
 
 cd %1
-llc -filetype=obj %2.ll
+echo Processing files:
+echo --------------------------------- 
+for %%f in (*.ll) do (
+    echo %1\%%~nf.ll
+    llc -filetype=obj %1\%%~nf.ll
+)
+echo ---------------------------------
+echo Linking ...
 link /DLL /out:sodium.dll *.obj MSVCRTD.lib User32.lib Gdi32.lib ole32.lib Comdlg32.lib OleAut32.lib
+
+cd C:\Sodium\Sodium-Setup
